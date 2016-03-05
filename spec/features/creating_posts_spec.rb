5128491201c0fe09338fc90a213778138d886f4e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-05 23:00:03 +0000

    test added for editing

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -18,14 +18,4 @@ feature 'Creating posts' do
     click_button 'Create Post'
     expect(page).to have_content('WHAT IS THIS WORLD? NO PICTURE?')
   end
-
-  feature 'Can view individual posts' do
-    scenario 'Can click and view a single post' do
-      post = create(:post)
-
-      visit '/'
-      find(:xpath, "//a[contains(@href,'posts/1')]").click
-      expect(page.current_path).to eq(post_path(post))
-    end
-  end
 end
