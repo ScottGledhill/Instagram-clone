807d8bcc7476692eb77470cb981bcfc739a41d3e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-05 20:13:45 +0000

    show view added for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,16 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    redirect_to @post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
   private
 
   def post_params
     params.require(:post).permit(:caption, :image)
   end
-  
+
 end
