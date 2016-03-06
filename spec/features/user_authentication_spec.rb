621e7bf23378eec1d5f6221f0a8dcfd038672d7a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-06 15:37:03 +0000

    patial view created

---
+++ spec/features/user_authentication_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User authentication' do
+  background do
+    create(:user)
+  end
+  scenario 'can log in from the index' do
+    visit '/'
+    expect(page).to_not have_content('New Post')
+    click_link 'Login'
+    fill_in 'Email', with: '1@2.3.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content('Signed in, well done on wasting your life')
+    expect(page).to_not have_content('Register')
+    expect(page).to have_content('Logout')
+  end
+end
