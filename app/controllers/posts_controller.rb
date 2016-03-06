cc2535f20acc139d65b506aac65ff5e43a710510
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-06 02:19:59 +0000

    destroy not working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    p @post
     redirect_to posts_path
   end
 
--- spec/features/deleting_posts_spec.rb
+++ spec/features/deleting_posts_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
 feature 'Deleting posts' do
-  background do
-    create(:post, caption: 'fml')
-    visit '/'
-    find('[alt="Coffee"]').click
-  end
 
   scenario 'Can delete a single post' do
+    add_pic
+    find('[alt="Coffee"]').click
     click_link 'Update Post'
     click_link 'Delete Post'
     expect(page).to have_content('deleted')
