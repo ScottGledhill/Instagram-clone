d4ff8a06a8f2bc70ddf2c8493ca595eb17675204
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-06 23:49:28 +0000

    test passed

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -4,7 +4,7 @@ feature 'Editing posts' do
   scenario 'Can edit a post' do
     add_pic
     find('[alt="Coffee"]').click
-    click_link 'Update Post'
+    click_link 'Edit Post'
     find('[type=text]').set 'I AM STUPID'
     click_button 'Update Post'
     expect(page).to have_content("I AM STUPID")
