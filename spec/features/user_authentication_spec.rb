5d04b834c29fea0b3c23f715a2c7f2dccb678a81
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-06 19:30:57 +0000

    test written for logging out

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -15,4 +15,14 @@ feature 'User authentication' do
     expect(page).to_not have_content('Register')
     expect(page).to have_content('Logout')
   end
+
+  scenario 'can log out once logged in' do
+   visit '/'
+   click_link 'Login'
+   fill_in 'Email', with: '1@2.3.com'
+   fill_in 'Password', with: 'password'
+   click_button 'Log in'
+   click_link 'Logout'
+   expect(page).to have_content('Well done, stay logged out..')
+ end
 end
