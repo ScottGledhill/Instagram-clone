860a4c4777fa4234c0849231c3970abf77cd4e88
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-06 15:54:57 +0000

    view changed for individual name, controller changed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "Great, another picture no one cares about."
       redirect_to @post
