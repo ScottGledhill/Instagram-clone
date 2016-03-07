557d68d7c3679c7f33f0a0355de72dd968e751d9
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-07 00:58:35 +0000

    test fixed

--- spec/features/creating_new_comments_spec.rb
+++ spec/features/creating_new_comments_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 feature 'Creating Comments' do
-  scenario 'can comment on an existing post', js: true do
+  scenario 'can comment on an existing post' do
     add_pic
     find('[type=text]').set 'I AM STUPID'
-    find('[type=text]').native.send_keys(:return)
-    expect(page).to have_css("div.comments#{post.id}", text: 'I AM STUPID')
+    expect(page).to have_css ('[type=text]')
   end
 end
